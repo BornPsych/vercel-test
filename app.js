dc69c553fe40cfb0a0b966966f87c009067f3b4f
Yogesh Shahi <65803503+BornPsych@users.noreply.github.com>
2024-03-10 15:19:15 +0530

    test 4

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const app = express()
 const PORT = 8000
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  if(process.env.HELLO){
+    const x = process.env.HELLO;
+ }else if(process.env.HELLO2){
+   const x = process.env.HELLO2;
+}if(process.env.HELLO3){
+ const x = process.env.HELLO3;
+}if(process.env.HELLO4){
+ const x = process.env.HELLO4;
+}
+  res.send(x)
 })
 
 app.get('/about', (req, res) => {
@@ -17,7 +26,7 @@ app.get('/about', (req, res) => {
 }if(process.env.HELLO4){
   const x = process.env.HELLO4;
 }
-  res.send('About route 🎉 ', x);
+  res.send('About route 🎉 ');
 })
 
 app.listen(PORT, () => {
